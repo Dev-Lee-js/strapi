b99b49bbe829fd65481053d10b80e0839757afeb
Lee.js <whdtlr202@gmail.com>
2022-09-19 14:38:02 +0000

    strapi mongoDB connecting

--- be/config/server.js
+++ be/config/server.js
@@ -1,7 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ac18d17c5f6de57d6461dfcd13010233'),
+    },
   },
 });
---
+++ be/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c23fee2b-0480-459e-b4e1-578260806a83'
+};
